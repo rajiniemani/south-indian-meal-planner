d73536d07c3ee5a8231d74b181871df113c8d48f
rajiniemani <emanirajini@example.com>
2025-05-01 00:05:27 -0400

    Fix syntax error in App.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import Contact from "./Contact";
 
 function App() {
   return (
-      <Helmet>
-       <script
-         data-ad-client="ca-pub-2510038568334237"
-         async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
-      </Helmet>
-      {<Router>
-      <Routes>
-        <Route path="/" element={<MealPlanner />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>}
-    </>
+    <>
+    <Helmet>
+    <script
+      data-ad-client="ca-pub-2510038568334237"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
+  </Helmet>
+    <Router>
+    <Routes>
+      <Route path="/" element={<MealPlanner />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+      </>
   );
 }
-
 export default App;
